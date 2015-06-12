9d46fb112025b5c7671253c2efeed92569049dcb
NateABaker <NateABaker@gmai.com>
2015-06-11 21:07:04 -0400

    Created simple solar system genorator

--- core/src/com/gdxjam/orion/utils/WorldGenerator.java
+++ core/src/com/gdxjam/orion/utils/WorldGenerator.java
@@ -1,5 +1,6 @@
 package com.gdxjam.orion.utils;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.gdxjam.orion.entities.Sattelite;
 import com.gdxjam.orion.entities.Sun;
@@ -38,8 +39,30 @@ public class WorldGenerator {
 	public void createSolarSystem() {
 		// The sun has a radius of 10
 		Sun sun = EntityFactory.createSun();
+		
+		
+		SatteliteParameters p1 = new SatteliteParameters(sun, 2, 30, 360, 10, 1);
+		Sattelite planet1 = EntityFactory.createSattelite(p1);
+		
+		SatteliteParameters p2 = new SatteliteParameters(sun, 2, -30, 360, 10, 1);
+		Sattelite planet2 = EntityFactory.createSattelite(p2);
+		
+		int planetsPerSide = (int)(MathUtils.random()*4)+4;
+		float dist = (MathUtils.random()*30)+20;
+		for(int i = 0; i < planetsPerSide; i++){
+			dist = dist + (MathUtils.random()*30)+20;
+			
+			SatteliteParameters j = new SatteliteParameters(sun, (MathUtils.random()*8)+2, dist, (MathUtils.random()*500)+10000, 10, 1);
+			Sattelite redPlanet = EntityFactory.createSattelite(j);
+			
+			dist = dist + (MathUtils.random()*30)+20;
+			
+			SatteliteParameters k = new SatteliteParameters(sun, (MathUtils.random()*8)+2, -dist, (MathUtils.random()*500)+10000, 10, 1);
+			Sattelite bluePlanet2 = EntityFactory.createSattelite(k);
+		}
 
-		// Create a planet and its moon !VERY SENSITIVE RELATIONSHIP!
+
+		/* Create a planet and its moon !VERY SENSITIVE RELATIONSHIP!
 		SatteliteParameters p1 = new SatteliteParameters(sun, 2, 30, 360, 10, 1);
 		Sattelite planet1 = EntityFactory.createSattelite(p1);
 
@@ -52,6 +75,7 @@ public class WorldGenerator {
 
 		SatteliteParameters m = new SatteliteParameters(planet2, 2, 12, 360, 10, 1f);
 		EntityFactory.createSattelite(m);
+		*/
 
 	}
 }
