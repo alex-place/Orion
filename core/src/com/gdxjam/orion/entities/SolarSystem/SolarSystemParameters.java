ee5d85a66aaaadc74968a7994a81c0b4a597930d
NateABaker <NateABaker@gmai.com>
2015-06-22 21:14:22 -0400

    this

--- core/src/com/gdxjam/orion/entities/SolarSystem/SolarSystemParameters.java
+++ core/src/com/gdxjam/orion/entities/SolarSystem/SolarSystemParameters.java
@@ -1,5 +1,17 @@
 package com.gdxjam.orion.entities.SolarSystem;
 
+import com.sun.xml.internal.stream.Entity;
+
 public class SolarSystemParameters {
+	private Entity parent;
+	private float distance, angelStep, angle, scale;
+	
+	public SolarSystemParameters(Entity parent, float distance, float angelStep, float angle, float scale){
+		this.parent = parent;
+		this.distance = distance;
+		this.angelStep = angelStep;
+		this.angle = angle;
+		this.scale = scale;
+	}
 
 }
